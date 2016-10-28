131521690898f6434295fb42308445730d9ec906
Christine Du <duyuqingecho@gmail.com>
2016-10-27 20:50:27 -0400

    change localhost

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express = require('express');
 
 // Create our app
 var app = express();
+
+// For heroku, http and https redirections. Environment variable will get the port number when heroku started
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  }else{
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  }else{
+    next();
   }
 });
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
