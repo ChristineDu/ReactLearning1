18485d5e2baf61530a0515f504526302cbdfe543
Christine Du <duyuqingecho@gmail.com>
2016-10-27 22:18:48 -0400

    add foundation style grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var{Link, IndexLink} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>paragraph</p>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are some location examples to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Toronto'>Toronto</Link>
+        </li>
+        <li>
+            <Link to='/?location=New York'>New York</Link>
+        </li>
+      </ol>
     </div>
   )
 };
